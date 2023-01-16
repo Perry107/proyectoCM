6fb8649c45c8aa7a1dd922440bccb62156b79866
alumno <alumno@MacBook-2.local>
2023-01-16 15:12:09 -0600

    side view 1

--- proyectoCM/ViewController.swift
+++ proyectoCM/ViewController.swift
@@ -5,15 +5,47 @@
 //  Created by alumno on 16/01/23.
 //
 
+import SideMenu
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        sideMenu.leftSide = true
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        SideMenuManager.default.addPanGestureToPresent(toView: view)
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapMenuButtom(){
+        present(sideMenu, animated: true)
+    }
 
 }
 
+class MenuController: UITableViewController{
+    private let menuItems: [String]
+    
+    init(with menuItems: [String]){
+        self.menuItems = menuItems
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
+    
